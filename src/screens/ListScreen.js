84da9cbff89e88fd13be1d93b9d53079ba4904f0
Soubhagya <soubhagyaranjanghs@gmail.com>
2021-02-10 23:16:38 +0530

    flatlist properties

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View, FlatList } from 'react-native';
 
 const ListScreen = () => {
-    return <Text>List Screen</Text>;
+    const friends = [
+        { name: 'Friend1' },
+        { name: 'Friend2' },
+        { name: 'Friend3' },
+        { name: 'Friend4' },
+        { name: 'Friend5' },
+    ];
+
+    return (
+        <FlatList 
+    		data={friends} 
+    		renderItem={({ item }) => {
+    			return <Text>{item.name}</Text>;
+    		}} 
+    	/>
+    );
 };
 
 const styles = StyleSheet.create({});
