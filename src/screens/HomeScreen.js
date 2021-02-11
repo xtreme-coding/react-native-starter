a743b1f977341c65846c667a206d2384f73e0d1b
Soubhagya <soubhagyaranjanghs@gmail.com>
2021-02-11 13:33:56 +0530

    added navigation to coponents screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
   return (
     <View>
       <Text style={styles.text}>Hello Developer</Text>
       <Button
         onPress={() => {
-          console.log("button pressed");
+          props.navigation.navigate("Components");
         }}
         title="Go to Components Demo"
       />
       <TouchableOpacity
         onPress={() => {
-          console.log("List Pressed");
+          props.navigation.navigate("List");
         }}
       >
         <Text>Go to List Demo</Text>
