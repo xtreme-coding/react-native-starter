e4886acb57414153aa02457ae0e02b227fb20dc6
Soubhagya <soubhagyaranjanghs@gmail.com>
2021-02-11 13:16:35 +0530

    added button

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import HomeScreen from './src/screens/HomeScreen';
-import ComponentsScreen from './src/screens/ComponentsScreen';
-import ListScreen from './src/screens/ListScreen';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import ComponentsScreen from "./src/screens/ComponentsScreen";
+import ListScreen from "./src/screens/ListScreen";
 
-const navigator = createStackNavigator({
+const navigator = createStackNavigator(
+  {
     Home: HomeScreen,
     Components: ComponentsScreen,
     List: ListScreen,
-}, {
-    initialRouteName: 'List',
+  },
+  {
+    initialRouteName: "Home",
     defaultNavigationOptions: {
-        title: 'App',
+      title: "App",
     },
-});
+  }
+);
 
 export default createAppContainer(navigator);
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,25 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
+import { log } from "react-native-reanimated";
 
 const HomeScreen = () => {
-    return <Text style={styles.text}>Hello Developer</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Hello Developer</Text>
+      <Button
+        onPress={() => {
+          console.log("button pressed");
+        }}
+        title="Go to Components Demo"
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    text: {
-        fontSize: 30
-    }
+  text: {
+    fontSize: 30,
+  },
 });
 
 export default HomeScreen;
