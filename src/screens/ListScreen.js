bbd90f3cb871dceeeb8d14a91b20ac187fa3bfb6
Soubhagya <soubhagyaranjanghs@gmail.com>
2021-02-11 10:49:39 +0530

    scrolling flatlist

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -2,25 +2,35 @@ import React from 'react';
 import { Text, StyleSheet, View, FlatList } from 'react-native';
 
 const ListScreen = () => {
-    const friends = [
-        { name: 'Friend1' },
-        { name: 'Friend2' },
-        { name: 'Friend3' },
-        { name: 'Friend4' },
-        { name: 'Friend5' },
-    ];
+        const friends = [
+            { name: 'Friend1' },
+            { name: 'Friend2' },
+            { name: 'Friend3' },
+            { name: 'Friend4' },
+            { name: 'Friend5' },
+            { name: 'Friend6' },
+            { name: 'Friend7' },
+            { name: 'Friend8' },
+            { name: 'Friend9' },
+        ];
 
-    return (
-        <FlatList
-        	keyExtractor={(friend) => {friend.name}} 
-    		data={friends} 
-    		renderItem={({ item }) => {
-    			return <Text>{item.name}</Text>;
+        return (
+                <FlatList
+	            horizontal
+	            showsHorizontalScrollIndicator={false}
+	        	keyExtractor={friend => friend.name} 
+	    		data={friends} 
+	    		renderItem={({ item }) => {
+	    			return <Text style={styles.textStyle}>{item.name}</Text>;
     		}} 
     	/>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    textStyle: {
+        marginVertical: 50
+    }
+});
 
 export default ListScreen;
