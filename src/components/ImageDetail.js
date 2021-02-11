b6233bfaf509452197167fe18498aa8fec6e0be0
Soubhagya <soubhagyaranjanghs@gmail.com>
2021-02-11 19:47:55 +0530

    added props from image screen to detail

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 
-const ImageDetail = () => {
-  return <Text>Image Detail</Text>;
+const ImageDetail = (props) => {
+  return <Text>{props.title}</Text>;
 };
 
 const styles = StyleSheet.create({});
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -5,9 +5,9 @@ import ImageDetail from "../components/ImageDetail";
 const ImageScreen = () => {
   return (
     <View>
-      <ImageDetail />
-      <ImageDetail />
-      <ImageDetail />
+      <ImageDetail title="Forest" />
+      <ImageDetail title="Beach" />
+      <ImageDetail title="Mountain" />
     </View>
   );
 };
