0bd1bbe5ba873116e0636ec7c64a283e8220ec86
Soubhagya <soubhagyaranjanghs@gmail.com>
2021-02-11 19:40:25 +0530

    added image detail

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const ImageDetail = () => {
+  return <Text>Image Detail</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageDetail;
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import ImageDetail from "../components/ImageDetail";
 
 const ImageScreen = () => {
-  return <Text>Image Screen</Text>;
+  return (
+    <View>
+      <ImageDetail />
+      <ImageDetail />
+      <ImageDetail />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
