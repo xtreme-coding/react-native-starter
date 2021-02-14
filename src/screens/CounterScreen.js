65e152e1b544f3c8e3fe978dca73e409f747d6de
Soubhagya <soubhagyaranjanghs@gmail.com>
2021-02-14 17:55:22 +0530

    added counter buttons

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -2,8 +2,13 @@ import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
 const CounterScreen = () => {
+  // todo: fix this
+  counter = 0;
+
   return (
     <View>
+      <Button title="Increase" />
+      <Button title="Decrease" />
       <Text>Current Count:</Text>
     </View>
   );
